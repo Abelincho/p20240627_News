f2f5a8a110b9dc6661e0c30b20868b7306ffa235
abelincho <elmonodelpatinete@gmail.com>
2024-06-28 17:42:52 +0200

    :D

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,7 @@ import { loadEnvFile } from "process";
 import elPaisSeeker from "./seekers/elPaisSeeker.js";
 import laVanguardiaSeeker from "./seekers/laVanguardiaSeeker.js";
 
-loadEnvFile();
+loadEnvFile('../.env');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
